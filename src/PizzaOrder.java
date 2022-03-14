30605777ef664632fff6a82b495ff6a8df91532c
logitechg502mouse <lader@sheridancollege.ca>
2022-03-14 09:26:17 -0400

    testinlol

---
+++ src/PizzaOrder.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PizzaOrder extends Application {
+    public static void main(String[] args){
+      launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+        primaryStage.setTitle("Demo JavaFX application");
+        Parent root = FXMLLoader.load(getClass().getResource("PizzaOrder.fxml"));
+        primaryStage.setScene(new Scene(root, 700, 700)); 
+
+        primaryStage.show();
+    }
+}
+
+//this is a test
