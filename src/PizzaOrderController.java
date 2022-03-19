4c7592a10702cc9b22b1a04ac2c3fc18cb6e7815
logitechg502mouse <lader@sheridancollege.ca>
2022-03-19 19:54:51 -0400

    hu

--- src/PizzaOrderController.java
+++ src/PizzaOrderController.java
@@ -3,16 +3,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class PizzaOrderController implements Initializable {
@@ -35,25 +34,21 @@ public class PizzaOrderController implements Initializable {
     @FXML
     private ChoiceBox<String> pizzaOrderChoice, pizzaSizeChoice;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         pizzaSizeChoice.getItems().addAll(pizzaSize);
         pizzaSizeChoice.setOnAction(this::orderListing);
-        pizzaSizeChoice.setValue(pizzaSize[0]);
+        pizzaSizeChoice.setValue(pizzaSize[0]);    
 
         pizzaOrderChoice.getItems().addAll(pizzaAmount);
         pizzaOrderChoice.setValue(pizzaAmount[0]);
         pizzaOrderChoice.setOnAction(this::orderListing);
 
-        ordr.setText("Size of Pizza:\nCrust Type:\nToppings:\nNumber of Pizza:");
-
-
-    
+        ordr.setText("Size of Pizza: "+ pizzaSizeChoice.getValue() +"\nCrust Type:\nToppings:\nNumber of Pizza:" + pizzaOrderChoice.getValue());    
         
     }
 
-    private String toppingsListing(){
+    public String toppingsListing(){
         String toppings = "";
         if(pepperoni.isSelected()){
             toppings += pepperoni.getText() + "\n";
@@ -104,30 +99,19 @@ public class PizzaOrderController implements Initializable {
 
     }
 
-    public void orderPizza() throws IOException{
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("PizzaOrderSecondWindow.fxml"));
-            Parent root = loader.load();
-    
-            //The following both lines are the only addition we need to pass the arguments
-            PizzaOrderSecondWindowController controller2 = loader.getController();
-    
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.setTitle("Layout2 + Controller2");
-            stage.show();
-    
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-     
+    public void orderPizza(ActionEvent e) throws IOException{
+        String pizzaOrder = ordr.getText();
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PizzaOrderSecondWindow.fxml"));
+        Parent root2 = loader.load();
+        PizzaOrderSecondWindowController secondWindowController = loader.getController();
+        secondWindowController.setLabelText(pizzaOrder);
+        Stage stage = new Stage();
+        Scene scene = new Scene(root2);
+        stage.setScene(scene);
+        stage.show();
     }
-
+        
 }
 
 //vscode://vscode.github-authentication/did-authenticate?windowid=1&code=6784e20069803ed742df&state=9fb38e2d-e6e1-4d74-a50e-04cf9d85eef3
-
-
-//TODO
-//it's not passing the stuff from one controller to the other
--- src/PizzaOrderSecondWindowController.java
+++ src/PizzaOrderSecondWindowController.java
@@ -1,31 +1,33 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
+
 
 public class PizzaOrderSecondWindowController implements Initializable{
 
     @FXML
-    private Label odrFinal;
+    private Label pizzaOrder;
 
-    @FXML
-    private ImageView pizzaView;
+    //I literally don't know what happen, it was broken before and all i did was change the label from ordFinal to pizzaOrder
 
+    public void setLabelText(String pizzaOrder){
+        this.pizzaOrder.setText(pizzaOrder);
+      
+    }
 
-    
-
-    // public void setLabelText(String m){
-    //     odrFinal.setText("amongus");
-    // }
-
-
-
+    public void add(int i, int x){
+        System.out.println(i + x);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // odrFinal.setText("ye");
+        // System.out.println(pizzaOrder.getText());  
+        // pizzaOrder.setText("testing");
+        // System.out.println(pizzaOrder.getText()); 
+        
     }
 
    
